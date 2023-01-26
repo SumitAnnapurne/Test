fa02f5e68566c72eb5326e0b83ca8ed41477d109
SumitAnnapurne <103450426+SumitAnnapurne@users.noreply.github.com>
2023-01-26 16:18:04 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+package com.velocity.demo;
+import java.util.*;
+public class Test {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList <String>();
+		list.add("java");
+		list.add("spring");
+		list.add("hibernate");
+		System.out.println(list);
+	
+		
+	}
+
+}
